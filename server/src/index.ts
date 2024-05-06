4221b392801d49e67ff53a992d8a6eaa4f99b5a8
Ethan Parent <ethan.parent1@gmail.com>
2024-05-06 18:36:33 -0500

    Get server talking to front end

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,36 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors'
+import OpenAI from 'openai'
+import 'dotenv/config'
 
 const app = express()
+//Change this later
+app.use(cors({ origin: '*', credentials: true }))
 const port = 3000
+
+const openai = new OpenAI()
+
+async function getAICompletion() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
+    model: 'gpt-3.5-turbo',
+  })
+
+  console.log(completion.choices[0])
+}
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, world!')
+})
+
+app.post('/api/analyze', (req: Request, res: Response) => {
+  try {
+    // getAICompletion()
+    res.send({ message: 'Status is ok', body: 'im the body' })
+  } catch (err) {
+    console.error('Error analyzing: ', err)
+    res.status(500).send('Error anlayzing text')
+  }
 })
 
 app.listen(port, () => {
